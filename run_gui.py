b87cd55812e6cf2ad4a8b234e37501490feafb4e
falkodem <dimbatsula@gmail.com>
2022-05-17 00:33:52 +0300

    upd

--- run_gui.py
+++ run_gui.py
@@ -1,5 +1,6 @@
 import os
 from kivy.app import App
+from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
@@ -8,8 +9,8 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.core.window import Window
 from kivy.clock import Clock
 
-Window.size = (1000, 400)
-Window.clearcolor = (6 / 255, 71 / 255, 57 / 255, 1)
+Window.size = (1100, 400)
+Window.clearcolor = (6 / 255, 2 / 255, 46 / 255, 1)
 
 
 class SysUI(App):
@@ -24,7 +25,10 @@ class SysUI(App):
             except FileNotFoundError:
                 pass
 
-
+        self.title_head = Button(text='Интерфейс системы речевого взаимодействия',
+                                 font_size='40dp', font_name='DejaVuSans',
+                                 size_hint=(1, 0.3),
+                                 background_color=(1, 2, 2), color=(0, 0, 0.2))
         self.swtch_act = Switch(active=False)
         self.swtch_chassis = Switch(active=False)
         self.swtch_rec = Switch(active=False)
@@ -32,18 +36,25 @@ class SysUI(App):
         self.switches = GridLayout()
         self.switches.cols = 2
         self.switches.rows = 3
-        self.switches.add_widget(Label(text="Система", font_size='25dp', font_name='Roboto-Bold'))
+        self.switches.add_widget(Button(text="Система", font_size='25dp', font_name='Roboto-BoldItalic',
+                                        background_color=(5, 2, 1.2), color=(0, 0, 0.2)))
         self.switches.add_widget(self.swtch_act)
-        self.switches.add_widget(Label(text="Шасси", font_size='25dp', font_name='Roboto-Bold'))
+        self.switches.add_widget(Button(text="Шасси", font_size='25dp', font_name='Roboto-BoldItalic',
+                                        background_color=(5,2,1.2), color= (0,0,0.2)))
         self.switches.add_widget(self.swtch_chassis)
-        self.switches.add_widget(Label(text="Запись", font_size='25dp', font_name='Roboto-Bold'))
+        self.switches.add_widget(Button(text="Запись", font_size='25dp', font_name='Roboto-BoldItalic',
+                                        background_color=(5,2,1.2), color= (0,0,0.2)))
         self.switches.add_widget(self.swtch_rec)
 
         self.info = BoxLayout()
         self.info.orientation = 'vertical'
-        self.curr_command = Label(text='*Команда отсутствует*', font_size='25dp', font_name='Roboto-Bold')
-        self.sys_answer = Label(text='*Ответ отсутствует*', font_size='25dp', font_name='Roboto-Bold')
+        self.curr_command = Label(text='*Команда отсутствует*', font_size='18dp', font_name='DejaVuSans')
+        self.sys_answer = Label(text='*Ответ отсутствует*', font_size='18dp', font_name='DejaVuSans')
+        self.info.add_widget(Button(text='Полученная команда', font_size='25dp', font_name='Roboto-BoldItalic',
+                                    background_color=(5, 2, 1.2), color=(0, 0, 0.2)))
         self.info.add_widget(self.curr_command)
+        self.info.add_widget(Button(text='Ответ системы', font_size='25dp', font_name='Roboto-BoldItalic',
+                                    background_color=(5, 2, 1.2), color=(0, 0, 0.2)))
         self.info.add_widget(self.sys_answer)
 
     def update(self, *args):
@@ -84,13 +95,18 @@ class SysUI(App):
             pass
 
     def build(self):
-
+        title = BoxLayout()
+        title.orientation = 'vertical'
         layout = BoxLayout()
         layout.orientation = 'horizontal'
+
+        title.add_widget(self.title_head)
+        title.add_widget(layout)
 
         layout.add_widget(self.info)
         layout.add_widget(self.switches)
         Clock.schedule_interval(self.update, 0.2)
-        return layout
+
+        return title
 
 SysUI().run()
